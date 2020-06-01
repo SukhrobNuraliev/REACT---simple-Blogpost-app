3fe935842d91a9a75bc679d69ba0208a011abcd7
Sukhrob Nuraliev <sukhrobnuraliev@mail.ru>
2020-06-01 16:22:20 +0500

    initial commit

---
+++ precache-manifest.23d17194d9033fa924743df50130eb1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b6bb08c8d7af948b41c2f7ee854b6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "081171145d5cf4bcf963",
+    "url": "/static/css/main.a9edb878.chunk.css"
+  },
+  {
+    "revision": "0d05757aa4a0b871a7d0",
+    "url": "/static/js/2.f1c12a1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f1c12a1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "081171145d5cf4bcf963",
+    "url": "/static/js/main.a0e9b10d.chunk.js"
+  },
+  {
+    "revision": "e980f59a9a376501375f",
+    "url": "/static/js/runtime-main.def2087a.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/static/media/pokeball.f10bf078.png"
+  }
+]);
